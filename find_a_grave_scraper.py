1c660ad534e47f8f7276a5cd9ba7e72fa88cb4a5
Phillip Post <phillip.post@gmail.com>
2022-03-24 12:34:13 -0400

    merging with branch

--- find_a_grave_scraper.py
+++ find_a_grave_scraper.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import requests
 import sys
+import json
 
 
 def get_file(first_name ="George", last_name="Washington", page="1"):
@@ -12,21 +13,21 @@ def get_file(first_name ="George", last_name="Washington", page="1"):
         "deathyear=&deathyearfilter=&location=&locationId=&memorialid=&mcid=&"
         f"linkedToName=&datefilter=&orderby=b&plot=&page={page}#sr-1075")
 
-    site_HTML = requests.get(url) # HTML from URL
+    page_HTML = requests.get(url) # HTML from URL
 
     # successful url request code is 200
-    if site_HTML.status_code != 200:
+    if page_HTML.status_code != 200:
         sys.exit("Connection Failed.") # stop execution and return error
 
-    return site_HTML
+    return page_HTML
 
 
 def get_info(first_name ="George", last_name="Washington"):
-    # get HTML of 1st page
-    site = get_file(first_name, last_name)
+    # get HTML of specified page number
+    page = get_file(first_name, last_name, 1)
 
     # Parse HTML into Beautiful Soup format
-    parsed = BeautifulSoup(site.content, "html.parser")
+    parsed = BeautifulSoup(page.content, "html.parser")
 
     #Find the div containing the maximum number of pages
     max_page = parsed.find(id = "gotoPage")
@@ -35,6 +36,17 @@ def get_info(first_name ="George", last_name="Washington"):
     #string of any element in the div to find its value
     max_page = max_page["max"]
 
+    #Create a list of all tag with an attribute class "memorial-item"
+    #tags act like containers that contain attributes and text
+    grave_infos = parsed.find_all(class_ = "memorial-item")
+
+    #select the first item of the list
+    grave_info = grave_infos[0]
+
+    #find the text contained within the <i> tag
+    name = grave_info.find("i").string
+
+    print(name)
     #TODO: get data for every page in range
     #TODO: try to limit file size
 
