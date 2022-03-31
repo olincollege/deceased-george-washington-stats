ef2c0e973a3cfebb8ae41308cdac5db5abb6febe
Hedonical <91704211+Hedonical@users.noreply.github.com>
2022-03-31 12:07:27 -0400

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,12 +1,4 @@
-def test_integers():
-    """
-    Check that a name with string integers still returns a dataset.
-    """
-    assert get_file("Wash23", "Ford2")
-
-def test_large_page_number():
-    """
-    Check that a page number that exceeds the number of web pages
-    returns an empty dataset.
-    """
-    assert get_file("George", "Washington", "2000000")
+# We did not have any unit tests due to the nature of our functions. All of
+# our functions either take or output HTML files or files containing HTML
+# elements making testing them exceeding difficult using the tools
+# we are expected to know for the scope of this project.
